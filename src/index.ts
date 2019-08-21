410d0bd5b86f68bbf41b074b6e5397e1a6593701
Chengliang <lianguasth@sina.com>
2019-08-20 23:04:19 -0700

    add async call to meeting agenda

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { WebClient } from "@slack/web-api";
 import puppeteer from "puppeteer";
 
 import { BPTM_ADMIN, BPTM_ADMIN_PWD } from "./puppeteer/global";
@@ -9,6 +10,77 @@ interface IAgendaCell {
   role: string;
   signed: boolean;
 }
+
+// const generateMessage = async (agenda: Map<string, IAgendaCell>) => {
+//   let text: string = "Hello Dear Toastmasters! \nThe agenda this week will be: \n ";
+//   const unassignedRoles: string[] = new Array();
+//   Object.values(agenda).forEach((a: IAgendaCell)  => {
+//     // tslint:disable-next-line:no-console
+//     console.log("role is: " + a.role);
+//     if (a.signed) {
+//        text += a.role + " : " + a.member + "\n";
+//     } else {
+//       unassignedRoles.push(a.role);
+//     }
+//   });
+
+//   if (unassignedRoles.length > 0) {
+//     text += "\n Please hurry up to fill in the following *unassigned* roles: \t";
+//     for (let i = 0; i < unassignedRoles.length; i++) {
+//       text += unassignedRoles[i];
+//       if (i < unassignedRoles.length - 1) {
+//         text += ", ";
+//       }
+//     }
+//   }
+//   return text;
+// };
+
+const sendSlackNotification = (agenda: Map<string, IAgendaCell>) => {
+
+  const token = process.env.SLACK_TOKEN;
+
+  const web = new WebClient(token);
+
+  const conversationId = "GMHHVSL5D";
+
+  Object.values(agenda).forEach((a: IAgendaCell)  => {
+    // tslint:disable-next-line:no-console
+    console.log("role is: " + a.role);
+  });
+
+  let text: string = "Hello Dear Toastmasters! \nThe agenda this week will be: \n ";
+  const unassignedRoles: string[] = new Array();
+  Object.values(agenda).forEach((a: IAgendaCell)  => {
+    // tslint:disable-next-line:no-console
+    console.log("role is: " + a.role);
+    if (a.signed) {
+       text += a.role + " : " + a.member + "\n";
+    } else {
+      unassignedRoles.push(a.role);
+    }
+  });
+
+  if (unassignedRoles.length > 0) {
+    text += "\n Please hurry up to fill in the following *unassigned* roles: \t";
+    for (let i = 0; i < unassignedRoles.length; i++) {
+      text += unassignedRoles[i];
+      if (i < unassignedRoles.length - 1) {
+        text += ", ";
+      }
+    }
+  }
+  // const text = await generateMessage(agenda);
+
+  (async () => {
+    // See: https://api.slack.com/methods/chat.postMessage
+    const res = await web.chat.postMessage({ channel: conversationId, text });
+    // `res` contains information about the posted message
+    // tslint:disable-next-line:no-console
+    console.log("Message sent: ", res.ts);
+  })();
+
+};
 
 (async () => {
   const browser = await puppeteer.launch({headless: false, devtools: true});
@@ -28,8 +100,8 @@ interface IAgendaCell {
 
   await page.waitFor(6000);
 
-  const agenda: IAgendaCell[] = new Array();
   await page.evaluate(() => {
+    const agenda: Map<string, IAgendaCell> = new Map();
     const tds = document.querySelectorAll(".agendaTable td:nth-child(2)");
     for (const td of tds) {
       const role = td.getElementsByTagName("b")[0].textContent;
@@ -43,23 +115,48 @@ interface IAgendaCell {
           memberName = memberWithName.textContent;
         }
       }
-      // // tslint:disable-next-line:no-console
+      // tslint:disable-next-line:no-console
       // console.log("role is: " + role + " member is: " + memberName);
-      agenda.push({
+      agenda.set(role, {
         member: memberName,
         role,
         signed,
       });
+      // tslint:disable-next-line:no-console
+      console.log("size is: " + agenda.size);
     }
+    // tslint:disable-next-line:no-console
+    console.log("size is: " + agenda.size);
+    return agenda;
+  }).then((agenda) => {
+    // tslint:disable-next-line:no-console
+    console.log("size is: " + agenda.size);
+    sendSlackNotification(agenda);
   });
+
   // tslint:disable-next-line:no-console
-  console.log("agenda is" + agenda);
+  // console.log("size is: " + meetingAgenda.size);
+  // Object.values(meetingAgenda).forEach((a: IAgendaCell)  => {
+  //   // tslint:disable-next-line:no-console
+  //   console.log("role is: " + a.role);
+  // });
+
+  // tslint:disable-next-line:no-console
+  // console.log("values are: ", meetingAgenda.values());
+  // send slack notification
+  // await
+
   await page.waitFor(requestTimeOut);
   //
   await page.screenshot({path: "test.png", fullPage: true});
 
   await browser.close();
 })();
+
+// (async () => {
+//   const meetingAgenda: IAgendaCell[] = new Array();
+//   await sendSlackNotification(meetingAgenda);
+// })();
 
 // import express from "express";
 
